e6662bc829b8af2ae1cccf0376cf947a00ca1171
fahmizainal17 <fahmizainal9@gmail.com>
2024-10-25 11:07:24 +0800

    feat:added components

--- app/component.py
+++ app/component.py
@@ -93,7 +93,7 @@ def page_style():
         # HTML and JavaScript to open YouTube in a new tab
         new_tab_button = """
         <a href="https://www.youtube.com/watch?v=VeUiVCb7ZmQ?si=GzSBUP3zs1hEkigI" target="_blank">
-            <button style="background-color: #00dadb; color: white; border: none; padding: 10px 20px; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
+            <button style="background-color: #ff6347; color: white; border: none; padding: 10px 20px; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
                 🎵 Play Music while Reading
             </button>
         </a>
